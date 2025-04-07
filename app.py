9d98106d9fba3c1bc3efed2153a6427c3d8437fb
Erik Gan Vormala <verik@Eriks-MacBook-Air.local>
2025-04-07 19:58:03 +0300

    Added the option to return to login page after a false attempt and fixed code format

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ def index():
 def new_item():
     return render_template("new_item.html")
 
-
-
 @app.route("/create_item", methods=["POST"])
 def create_item():
     item_name=request.form["item_name"] 
@@ -44,37 +42,41 @@ def create():
     password1=request.form["password1"]
     password2=request.form["password2"]
     if password1 != password2:
-        return "VIRHE: Passwords do not match"
+        return "Error: Passwords do not match"
     password_hash=generate_password_hash(password1)
 
     try:
         sql="INSERT INTO users (username, password_hash) VALUES (?, ?)"
         db.execute(sql, [username, password_hash])
     except sqlite3.IntegrityError:
-        return "VIRHE: Username is occupied"
-
-    return "Sign in successful!"
-
+        return "Error: Username is occupied"
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "GET":
+    if request.method=="GET":
         return render_template("login.html")
-    if request.method == "POST":
+
+    if request.method=="POST":
         username=request.form["username"]
         password=request.form["password"]
 
         sql="SELECT id, password_hash FROM users WHERE username=?"
-        result=db.query(sql, [username])[0]
-        user_id=result["id"]
-        password_hash=result["password_hash"]
+        result=db.query(sql, [username])
+
+        if not result:
+            return render_template("401.html")
+
+        user=result[0]
+        user_id=user["id"]
+        password_hash=user["password_hash"]
 
         if check_password_hash(password_hash, password):
             session["user_id"]=user_id
             session["username"]=username
             return redirect("/")
         else:
-            return "VIRHE: väärä tunnus tai salasana"
+            return render_template("401.html")
 
 @app.route("/logout")
 def logout():
