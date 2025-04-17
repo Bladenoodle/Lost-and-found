203196fe51defc9854550bd3172042b702ab52cd
Erik Gan Vormala <verik@Eriks-MacBook-Air.local>
2025-04-17 17:45:08 +0200

    Optimize db.py

--- db.py
+++ db.py
@@ -3,20 +3,23 @@ from flask import g
 
 def get_connection():
     con = sqlite3.connect("database.db")
-    con.execute("PRAGMA foreign_keys=ON")
+    con.execute("PRAGMA foreign_keys = ON")
     con.row_factory = sqlite3.Row
     return con
 
 def execute(sql, params=[]):
     con = get_connection()
-    result = con.execute(sql, params)
-    con.commit()
-    g.last_insert_id = result.lastrowid
-    con.close()
+    try:
+        result = con.execute(sql, params)
+        con.commit()
+    except sqlite3.OperationalError as e:
+        print("SQLite OperationalError:", e)
+    finally:
+        con.close() 
 
 def last_insert_id():
-    return g.last_insert_id    
-    
+    return getattr(g, "last_insert_id", None)
+
 def query(sql, params=[]):
     con = get_connection()
     result = con.execute(sql, params).fetchall()
