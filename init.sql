6f795546fa1010397f19adc8cb3f844c94310992
Erik Gan Vormala <verik@Eriks-MacBook-Air.local>
2025-04-23 13:56:01 +0300

    Fix init.sql syntax for Windows error

--- init.sql
+++ init.sql
@@ -1,12 +1,15 @@
 DELETE FROM classes;
+/*
+in this file '-symbol is used instead of "-symbol,
+because "-symbol causes unexpected errors for windows users
+*/
+INSERT INTO classes (class_name, value) VALUES ('When', 'Today');
+INSERT INTO classes (class_name, value) VALUES ('When', 'This week');
+INSERT INTO classes (class_name, value) VALUES ('When', 'This month');
+INSERT INTO classes (class_name, value) VALUES ('When', 'This year');
+INSERT INTO classes (class_name, value) VALUES ('When', 'More than a year');
 
-INSERT INTO classes (class_name, value) VALUES ("When", "Today");
-INSERT INTO classes (class_name, value) VALUES ("When", "This week");
-INSERT INTO classes (class_name, value) VALUES ("When", "This month");
-INSERT INTO classes (class_name, value) VALUES ("When", "This year");
-INSERT INTO classes (class_name, value) VALUES ("When", "More than a year");
-
-INSERT INTO classes (class_name, value) VALUES ("Where", "City Centre Campus");
-INSERT INTO classes (class_name, value) VALUES ("Where", "Kumpula Campus");
-INSERT INTO classes (class_name, value) VALUES ("Where", "Meilahti Campus");
-INSERT INTO classes (class_name, value) VALUES ("Where", "Viikki Campus");
+INSERT INTO classes (class_name, value) VALUES ('Where', 'City Centre Campus');
+INSERT INTO classes (class_name, value) VALUES ('Where', 'Kumpula Campus');
+INSERT INTO classes (class_name, value) VALUES ('Where', 'Meilahti Campus');
+INSERT INTO classes (class_name, value) VALUES ('Where', 'Viikki Campus');
